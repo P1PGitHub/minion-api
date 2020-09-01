90980fdc36aded3126399e540faab3bde1d0a6f4
fishink-python <31522303+fishink-python@users.noreply.github.com>
2020-09-01 12:03:02 -0400

    Production changes

--- minionapi/minionapi/settings.py
+++ minionapi/minionapi/settings.py
@@ -27,9 +27,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'qba5%6mo#f6x!%$8kzy_cl8^b#u&$zxg83vkexrq)&lf9i)2f+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "minion-api.herokuapp.com"
+]
 
 
 # Application definition
@@ -95,18 +99,18 @@ WSGI_APPLICATION = 'minionapi.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv("PG_NAME"),
-        'USER': os.getenv("PG_USER"),
-        'PASSWORD': os.getenv("PG_PASS"),
-        'HOST': os.getenv("PG_HOST"),
-        'PORT': os.getenv("PG_PORT"),
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': os.getenv("PG_NAME"),
+    #     'USER': os.getenv("PG_USER"),
+    #     'PASSWORD': os.getenv("PG_PASS"),
+    #     'HOST': os.getenv("PG_HOST"),
+    #     'PORT': os.getenv("PG_PORT"),
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
@@ -146,6 +150,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 MEDIA_URL = "/media/"
