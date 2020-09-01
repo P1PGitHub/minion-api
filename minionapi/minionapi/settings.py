ca0b1dfd3dc4a9d321b6d430f7e766fa6761ac1d
fishink-python <31522303+fishink-python@users.noreply.github.com>
2020-09-01 12:49:19 -0400

    Turn debug on for testing on live server.

--- minionapi/minionapi/settings.py
+++ minionapi/minionapi/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'qba5%6mo#f6x!%$8kzy_cl8^b#u&$zxg83vkexrq)&lf9i)2f+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "localhost",
