c6fd9193b949b5c77f9b78aa261e64edf1be014d
fishink-python <31522303+fishink-python@users.noreply.github.com>
2020-09-08 17:16:29 -0400

    feat: tweaked account admin.

--- minionapi/accounts/admin.py
+++ minionapi/accounts/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from . import models
 
-admin.site.register(models.Account)
+
+class AccountAdmin(UserAdmin):
+    pass
+
+
+admin.site.register(models.Account, AccountAdmin)
