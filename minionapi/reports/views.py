3dad9cd71fc111dae4479359d187c5495609df0e
Devin Sharpe <devinrsharpe@gmail.com>
2020-09-28 08:18:49 -0400

    check in

--- minionapi/reports/views.py
+++ minionapi/reports/views.py
@@ -1,10 +1,17 @@
 import base64
 from datetime import datetime
 import os
+from shutil import copyfile
 
+import dateutil.parser
+from django.apps import apps
 from django.conf import settings
 from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404
+from django.template.defaultfilters import slugify
+from openpyxl import load_workbook
+from openpyxl.styles import Alignment, Color
+from openpyxl.styles.fills import PatternFill
 from rest_framework import generics
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.parsers import FileUploadParser, MultiPartParser
@@ -65,8 +72,11 @@ class CustomerServiceList(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         report = serializer.save(author=self.request.user)
         if not report.draft:
+            static_path = os.path.join(settings.BASE_DIR, 'staticfiles/xlsx/csqr/xlxs')
+            print(static_path)
+            print(static_path.isfile())
             send_mail(f"New CSQR for {report.company_name}", "Hello There",
-                      None, ["devin.s@priority1pos.com", "alyssa@priority1pos.com"])
+                      None, ["devin.s@priority1pos.com"])
 
 
 class CustomerServiceSimpleDraftsList(generics.ListAPIView):
@@ -110,11 +120,99 @@ class CustomerServiceRetrieveUpdate(generics.RetrieveUpdateAPIView):
     queryset = models.CustomerService.objects.all()
 
     def update(self, request, *args, **kwargs):
-        report = super().update(request, *args, **kwargs)
-        print(report)
-        if not report.draft:
-            send_mail(f"New CSQR for {report.company_name}", "Hello There",
-                      None, ["devin.s@priority1pos.com", "alyssa@priority1pos.com"])
+        update_response = super().update(request, *args, **kwargs)
+        report = update_response.data
+        
+        if not report["draft"]:
+            user_model = apps.get_model("accounts", "Account")
+            template_spread = os.path.join(settings.BASE_DIR, 'staticfiles/xlsx/csqr.xlsx')
+            report_dir = os.path.join(settings.MEDIA_ROOT, f"reports/csqr/")
+            if not os.path.exists(report_dir):
+                os.makedirs(report_dir)
+            new_spread = os.path.join(report_dir, f"{report['id']}.xlsx")
+            copyfile(template_spread, new_spread)
+            wb = load_workbook(filename = new_spread)
+            ws = wb.active
+
+            ws["D4"] = datetime.now() # Date
+            ws["A7"] = report["company_name"]
+            ws["C7"] = report["service_type"]
+            ws["A8"] = report["location"]
+            ws["C8"] = report["description"]
+            ws["A9"] = report["client_name"]
+
+            row = 13
+            for record in report["time_records"]:
+                ws.insert_rows(row)
+                ws.merge_cells(f"D{row}:F{row}")
+
+                start = dateutil.parser.parse(record["start"])
+                end = dateutil.parser.parse(record["end"])
+
+                ws[f"A{row}"] = start.strftime("%x")
+                ws[f"B{row}"] = start.strftime("%X")
+                ws[f"C{row}"] = end.strftime("%X")
+
+                employee_string = ""
+                for employee in record["users"]:
+                    employee_account = user_model.objects.get(id=employee)
+                    employee_string += f"{employee_account.first_name} {employee_account.last_name}, "
+                ws[f"D{row}"] = employee_string[:len(employee_string)-2]
+                
+                total_hours = (end - start).total_seconds()/3600
+                ws[f"G{row}"] = round(total_hours, 2)
+
+                row += 1
+
+            row += 3
+            items = {}
+            for item in report["inventory_checkouts"]:
+                ws.insert_rows(row)
+                ws.merge_cells(f"A{row}:B{row}")
+                ws.merge_cells(f"C{row}:D{row}")
+                ws.merge_cells(f"E{row}:G{row}")
+
+                ws[f"A{row}"] = item["description"]
+                ws[f"C{row}"] = item["model"]
+                ws[f"E{row}"] = item["serial"]
+
+                if item["model"] in items:
+                    items[item["model"]]["quantity"] += 1
+                else:
+                    items[item["model"]] = {"description": item["description"], "quantity": 1}
+
+                row += 1
+
+            row += 3
+            for model in items:
+                ws.insert_rows(row)
+                ws.merge_cells(f"A{row}:B{row}")
+                ws.merge_cells(f"D{row}:D{row}")
+                ws.merge_cells(f"G{row}:G{row}")
+
+                ws[f"A{row}"] = items[model]["description"]
+                ws[f"D{row}"] = model
+                ws[f"G{row}"] = items[model]["quantity"]
+
+                row += 1
+            
+            row += 2
+
+            ws[f"A{row}"] = report["summary"]
+            ws[f"A{row}"].alignment = Alignment(wrap_text=True)
+            ws.merge_cells(f"A{row}:G{row+4}")
+
+            row += 7
+            
+            selectedColor = Color(rgb="FF0000")
+            selectedFill = PatternFill(patternType="solid", fgColor=selectedColor)
+
+            ws[f"A{row}"].fill = selectedFill
+
+            wb.save(new_spread)
+            # send_mail(f"New CSQR for {report['company_name']}", "Hello There",
+            #           None, ["devin.s@priority1pos.com", "alyssa@priority1pos.com"])
+        return update_response
 
 
 class CustomerServiceSimpleList(generics.ListAPIView):
