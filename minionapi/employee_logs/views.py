4fb43ba62b8a6b4c6274ef4ca72453838acbb044
Devin Sharpe <devinsharpe@Devins-MacBook-Pro.local>
2020-10-10 11:58:49 -0400

    feat: change serializer for work entries lits create view from simple to full serializer.

--- minionapi/employee_logs/views.py
+++ minionapi/employee_logs/views.py
@@ -9,18 +9,14 @@ from . import serializers
 
 class WorkEntryListCreate(generics.ListCreateAPIView):
 
+    serializer_class = serializers.WorkEntrySerializer
+
     def get_queryset(self):
         request_date = datetime.strptime(self.kwargs.get(
             "date"), "%Y%m%d%H%M")
         request_end = request_date + timedelta(days=1)
 
         return models.WorkEntry.objects.filter(start__gte=request_date).filter(end__lte=request_end).filter(user=self.request.user).order_by("-start")
-
-    def get_serializer_class(self):
-        if (self.request.method == "GET"):
-            return serializers.WorkEntrySimpleSerializer
-        else:
-            return serializers.WorkEntrySerializer
 
 
 class WorkEntryCreate(generics.CreateAPIView):
