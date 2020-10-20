46e411450f6fc69d123d7e6ae31bf38eb3278c9a
Devin Sharpe <devinsharpe@Devins-MacBook-Pro.local>
2020-10-20 02:07:57 -0400

    feat: added new urls and views to employee logs for pulling team work entries.

--- minionapi/employee_logs/urls.py
+++ minionapi/employee_logs/urls.py
@@ -5,11 +5,14 @@ from . import views
 app_name = "employee_logs"
 urlpatterns = [
     path("entry/", views.WorkEntryCreate.as_view(), name="entry-create"),
+    path("entry/team/<start>/<end>/",
+         views.WorkEntryTeamList.as_view(), name="entry-team-list"),
     path("entry/<date>/", views.WorkEntryList.as_view(),
          name="entry-list-create"
-    ),
+         ),
     path("entry/range/<start>/<end>/", views.WorkEntryRangeList.as_view(),
          name="entry-list-range-create"
-    ),
-    path("entry/id/<pk>/", views.WorkEntryRetrieveUpdateDestroy.as_view(), name="entry-retrieve-update-destroy")
+         ),
+    path("entry/id/<pk>/", views.WorkEntryRetrieveUpdateDestroy.as_view(),
+         name="entry-retrieve-update-destroy")
 ]
--- minionapi/employee_logs/views.py
+++ minionapi/employee_logs/views.py
@@ -1,10 +1,12 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
+from django.apps import apps
 from django.shortcuts import render
 from rest_framework import generics
 
 from . import models
 from . import serializers
+from accounts import serializers as account_serializers
 
 
 class WorkEntryList(generics.ListAPIView):
@@ -13,7 +15,7 @@ class WorkEntryList(generics.ListAPIView):
 
     def get_queryset(self):
         request_date = datetime.strptime(self.kwargs.get(
-            "date"), "%Y%m%d%H%M")
+            "date"), "%Y%m%d%H%M").replace(tzinfo=timezone.utc)
         request_end = request_date + timedelta(days=1)
 
         return models.WorkEntry.objects.filter(start__gte=request_date).filter(end__lte=request_end).filter(user=self.request.user).order_by("-start")
@@ -25,9 +27,9 @@ class WorkEntryRangeList(generics.ListAPIView):
 
     def get_queryset(self):
         request_start = datetime.strptime(self.kwargs.get(
-            "start"), "%Y%m%d%H%M")
+            "start"), "%Y%m%d%H%M").replace(tzinfo=timezone.utc)
         request_end = datetime.strptime(self.kwargs.get(
-            "end"), "%Y%m%d%H%M") + timedelta(days=1)
+            "end"), "%Y%m%d%H%M").replace(tzinfo=timezone.utc) + timedelta(days=1)
 
         return models.WorkEntry.objects.filter(start__gte=request_start).filter(end__lte=request_end).filter(user=self.request.user).order_by("-start")
 
@@ -41,3 +43,36 @@ class WorkEntryRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
 
     serializer_class = serializers.WorkEntrySerializer
     queryset = models.WorkEntry.objects.all()
+
+
+class WorkEntryTeamList(generics.ListAPIView):
+
+    serializer_class = account_serializers.AccountSimpleSerializer
+
+    def get_queryset(self):
+        return apps.get_model("accounts", "account").objects.filter(team=self.request.user.team)
+
+    def list(self, request, *args, **kwargs):
+        res = super().list(request, *args, **kwargs)
+        request_start = datetime.strptime(
+            self.kwargs.get("start"), "%Y%m%d%H%M"
+        ).replace(tzinfo=timezone.utc)
+        request_end = datetime.strptime(self.kwargs.get(
+            "end"), "%Y%m%d%H%M"
+        ).replace(tzinfo=timezone.utc) + timedelta(days=1)
+
+        for index, user in enumerate(res.data):
+            user_obj = apps.get_model("accounts", "account").objects.get(
+                pk=user["id"]
+            )
+            res.data[index].update({"work_entries": serializers.WorkEntrySimpleSerializer(
+                models.WorkEntry.objects.filter(
+                    user=user_obj.id
+                ).filter(
+                    start__gte=request_start
+                ).filter(
+                    end__lte=request_end
+                ).order_by("-start"),
+                many=True
+            ).data})
+        return res
